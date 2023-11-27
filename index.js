1e21efb38b3f5c5f955ac80d527a939fc869d1c5
Paras <paraskamaliya5@gmail.com>
2023-11-27 20:43:11 +0530

    cors update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const { userRouter } = require("./Router/userRouter");
 require("dotenv").config();
 const app = express();
+app.use(cors());
 app.use(express.json())
 app.use("/users", userRouter);
 
